744891aaaa2861af447fcc525c593efe793e2fc8
patrickrochemn <77136808+patrickrochemn@users.noreply.github.com>
2022-02-21 13:28:22 -0600

    Update renderer.js

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,3 +1,24 @@
+/*
+
+Slides:
+    -Slide 0: Rectangle            [x]
+    -Slide 1: Circle               []
+    -Slide 2: Bezier curve         []
+    -Slide 3: Draw your name       []
+
+User controlled Options:
+    -Number of sections to divide circles/curves into
+        -Circle            []
+        -Bezier curve      []
+        -Draw your name    []
+    -Ability to show / hide points used in drawing routines
+        -Rectangle            [x]
+        -Circle               []
+        -Bezier curve         []
+        -Draw your name       []
+
+*/
+
 class Renderer {
     // canvas:              object ({id: __, width: __, height: __})
     // num_curve_sections:  int
@@ -46,7 +67,7 @@ class Renderer {
 
     // ctx:          canvas context
     drawSlide0(ctx) {
-        
+        this.drawRectangle(({x:100, y:100}), ({x:500, y:500}), [0, 0, 255, 255], ctx);
     }
 
     // ctx:          canvas context
@@ -69,7 +90,67 @@ class Renderer {
     // color:        array of int [R, G, B, A]
     // ctx:          canvas context
     drawRectangle(left_bottom, right_top, color, ctx) {
-        
+        var left_top = ({x:left_bottom.x, y:right_top.y});
+        var right_bottom = ({x:right_top.x, y:left_bottom.y});
+        /*
+        console.log(left_bottom);
+        console.log(left_top);
+        console.log(right_top);
+        console.log(right_bottom);
+        */
+        // vertices: (x0, y0), (x0, y1), (x1, y1), (x1, y0)
+        // lines: [(x0, y0), (x0, y1)], [(x0, y1), (x1, y1)], [(x1, y1), (x1, y0)], [(x0, y0), (x1, y0)]
+        //                  left                top                     right               bottom
+        this.drawLine(left_bottom, left_top, [0, 255, 0, 255], ctx);
+        this.drawLine(left_top, right_top,[0, 255, 0, 255], ctx);
+        this.drawLine(right_bottom, right_top,[0, 255, 0, 255], ctx);
+        this.drawLine(left_bottom, right_bottom,[0, 255, 0, 255], ctx);
+        if(this.show_points) {
+            // bottom left
+
+            // left
+            this.drawLine({x:left_bottom.x - 5, y:left_bottom.y - 5}, {x:left_bottom.x - 5, y:left_bottom.y + 5}, [0, 0, 255, 255], ctx);
+            // top
+            this.drawLine({x:left_bottom.x - 5, y:left_bottom.y + 5}, {x:left_bottom.x + 5, y:left_bottom.y + 5}, [0, 0, 255, 255], ctx);
+            // right
+            this.drawLine({x:left_bottom.x + 5, y:left_bottom.y + 5}, {x:left_bottom.x + 5, y:left_bottom.y - 5}, [0, 0, 255, 255], ctx);
+            // bottom
+            this.drawLine({x:left_bottom.x - 5, y:left_bottom.y - 5}, {x:left_bottom.x + 5, y:left_bottom.y - 5}, [0, 0, 255, 255], ctx);
+
+
+            // top left
+
+            // left
+            this.drawLine({x:left_top.x - 5, y:left_top.y - 5}, {x:left_top.x - 5, y:left_top.y + 5}, [0, 0, 255, 255], ctx);
+            // top
+            this.drawLine({x:left_top.x - 5, y:left_top.y + 5}, {x:left_top.x + 5, y:left_top.y + 5}, [0, 0, 255, 255], ctx);
+            // right
+            this.drawLine({x:left_top.x + 5, y:left_top.y + 5}, {x:left_top.x + 5, y:left_top.y - 5}, [0, 0, 255, 255], ctx);
+            // bottom
+            this.drawLine({x:left_top.x - 5, y:left_top.y - 5}, {x:left_top.x + 5, y:left_top.y - 5}, [0, 0, 255, 255], ctx);
+
+            // top right
+
+            // left
+            this.drawLine({x:right_top.x - 5, y:right_top.y - 5}, {x:right_top.x - 5, y:right_top.y + 5}, [0, 0, 255, 255], ctx);
+            // top
+            this.drawLine({x:right_top.x - 5, y:right_top.y + 5}, {x:right_top.x + 5, y:right_top.y + 5}, [0, 0, 255, 255], ctx);
+            // right
+            this.drawLine({x:right_top.x + 5, y:right_top.y + 5}, {x:right_top.x + 5, y:right_top.y - 5}, [0, 0, 255, 255], ctx);
+            // bottom
+            this.drawLine({x:right_top.x - 5, y:right_top.y - 5}, {x:right_top.x + 5, y:right_top.y - 5}, [0, 0, 255, 255], ctx);
+
+            // bottom right
+
+            // left
+            this.drawLine({x:right_bottom.x - 5, y:right_bottom.y - 5}, {x:right_bottom.x - 5, y:right_bottom.y + 5}, [0, 0, 255, 255], ctx);
+            // top
+            this.drawLine({x:right_bottom.x - 5, y:right_bottom.y + 5}, {x:right_bottom.x + 5, y:right_bottom.y + 5}, [0, 0, 255, 255], ctx);
+            // right
+            this.drawLine({x:right_bottom.x + 5, y:right_bottom.y + 5}, {x:right_bottom.x + 5, y:right_bottom.y - 5}, [0, 0, 255, 255], ctx);
+            // bottom
+            this.drawLine({x:right_bottom.x - 5, y:right_bottom.y - 5}, {x:right_bottom.x + 5, y:right_bottom.y - 5}, [0, 0, 255, 255], ctx);
+        }
     }
 
     // center:       object ({x: __, y: __})
